e1fa79ec9f9e92af7295a9f4677e490b612c2ba1
Isabel <iarcones@gmail.com>
2019-01-09 16:10:59 -0800

    testing with Heroku

--- public/assets/js/googlelogin.js
+++ public/assets/js/googlelogin.js
@@ -1,18 +1,10 @@
-
     var express = require("express");
 
     var router = express.Router();
     
     // Import the model (burger.js) to use its database functions.
     var db = require("../models");
-    
-    
-    
-        var express = require("express");
-    
-        var router = express.Router();
-    
-        var db = require("../models");
+
     
         // // // hide <div> with class of well on index.html when page loads
         // $(".well").hide();
