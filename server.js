eac90d438c3f797ab4bcb4ae7004ff40aeb58677
Isabel <iarcones@gmail.com>
2019-01-12 17:16:09 -0800

    test

--- server.js
+++ server.js
@@ -38,14 +38,14 @@ app.use(routes);
 //   // Log (server-side) when our server has started
 //   console.log("Server listening on: http://localhost:" + PORT);
 // });
-// db.sequelize.sync({ force: true }).then(function() {
-//   app.listen(PORT, function() {
-//     console.log("App listening on PORT " + PORT);
-//   });
-// });
-
-db.sequelize.sync().then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
 });
+
+// db.sequelize.sync().then(function() {
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
